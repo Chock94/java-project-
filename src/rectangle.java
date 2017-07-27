92781302882bd62cbbeecdd8299d55b643ecd381
Chock94 <teran.hotcackes@gmail.com>
2017-07-27 15:33:41 +0000

    corrections of grammar

--- src/rectangle.java
+++ src/rectangle.java
@@ -1,8 +1,8 @@
-public class Rectangle {
+public class rectangle {
 	public int length;
 	public int width;
 
-	public Rectangle(int length, int width) {
+	public rectangle(int length, int width) {
 	 this.length = length;
 	 this.width = width;
 	}
